9c40f5ab4e956258ac229d02611b5f33d1a608e0
Ibrahim <ibz_374@hotmail.com>
2024-10-01 15:03:38 +0100

    created navbar component and api functions for the fetching requests

---
+++ exhibition-curator-project/src/app/components/navbar.js
@@ -0,0 +1,30 @@
+'use client'
+import { useState } from "react";
+import Link from "next/link";
+
+export default function Navbar() {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen)
+    }
+
+    return (
+        <nav className="flex items-center justify-center p-8 bg-gray-800 text-white relative">
+            <button className="block md:hidden focus:outline-none" onClick={toggleMenu}>
+                {isOpen ? 'X' : '☰'}
+            </button>
+            <ul className={`md:flex md:space-x-12 ${isOpen ? 'block' : 'hidden'} absolute md:static bg-gray-800 md:bg-transparent w-full md:w-auto top-14 md:top-auto text-center`}>
+                <li className="py-2 hover:underline">
+                    <Link href="/" className="block">Home</Link>
+                </li>
+                <li className="py-2 hover:underline">
+                    <Link href="/page1" className="block">Page1</Link>
+                </li>
+                <li className="py-2 hover:underline">
+                    <Link href="/page2" className="block">Page2</Link>
+                </li>
+            </ul>
+        </nav>
+    )
+}
---
+++ exhibition-curator-project/src/app/utils/apiFunctions.js
@@ -0,0 +1,27 @@
+const harvard_API_KEY = '57c592c3-71a8-4c92-bda9-a4b0215968ec';
+
+export const fetchImagesFromHarvard = async() => {
+    const url = `https://api.harvardartmuseums.org/object?apikey=${harvard_API_KEY}&size=100`;
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        const objectsWithImages = data.records.filter(record => record.imagecount > 0);        
+        // console.log(objectsWithImages);
+        return objectsWithImages
+    } catch (error) {
+        console.log(error);
+        
+    }
+}
+
+
+export const fetchPromises = () => {
+    const objectIDs = [317877, 438003, 551786, 436492, 472562] // add all the object IDs
+  
+    return Promise.all(
+      objectIDs.map(id =>
+        fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${id}`)
+          .then(response => response.json())
+      )
+    );
+  };
